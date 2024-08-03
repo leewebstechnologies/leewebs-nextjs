a769c405ca68107f56e2cf2701caa129d634767b
leewebstechnologies <aliuajibade317@gmail.com>
2024-08-03 22:40:10 +0100

    add hero image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import hero from "../images/hero.png";
 
 export default function Home() {
-  return <div>Home</div>;
+  return <div>
+    <Image src={hero} alt="hero" className={styles.img} />
+  </div>;
 }
+ 
