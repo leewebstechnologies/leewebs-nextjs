2a3a4ace84784896c5dc96587ed71a40ea155695
leewebstechnologies <aliuajibade317@gmail.com>
2024-08-04 23:18:03 +0100

    add contact

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,21 @@
 import styles from "./page.module.css";
 
 const Contact = () => {
-  return <div className={styles.container}>Contact</div>;
+  return (
+    <>
+      <div className={styles.container}>
+        <h1>Let's Keep in Touch</h1>
+        <div className={styles.content}>
+          <div className={styles.imgContainer}>
+
+          </div>
+          <form action="">
+            
+          </form>
+
+        </div>
+      </div>
+    </>
+  );
 };
 export default Contact;
