519a3c77061346ae8746842f95244c7f5d536e65
leewebstechnologies <aliuajibade317@gmail.com>
2024-08-08 22:48:28 +0100

    add blog

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 import Image from "next/image";
 
 async function getData() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+  const res = await fetch("http://localhost:3000/api/posts", {
     cache: "no-store",
   });
 
@@ -19,10 +19,10 @@ const Blog = async () => {
   return (
     <div className={styles.mainContainer}>
       {data.map((item) => (
-        <Link href="/blog/testId" className={styles.container} key={item.id}>
+        <Link href="/blog/testId" className={styles.container} key={item._id}>
           <div className={styles.imgContainer}>
             <Image
-              src="https://images.unsplash.com/photo-1490730141103-6cac27aaab94?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              src={item.img}
               alt="masjid"
               width={400}
               height={250}
@@ -31,7 +31,7 @@ const Blog = async () => {
           </div>
           <div className={styles.content}>
             <h1 className={styles.title}>{item.title}</h1>
-            <p className={styles.description}>Description</p>
+            <p className={styles.description}>{item.desc}</p>
           </div>
         </Link>
       ))}
